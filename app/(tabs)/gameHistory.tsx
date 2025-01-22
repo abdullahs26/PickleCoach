0ed1fccbde62d57cc28d6f19d7c96b176a6d2c82
evanferns <evanferns9@gmail.com>
2025-01-22 15:43:54 -0500

    Added game history tab

--- app/(tabs)/gameHistory.tsx
+++ app/(tabs)/gameHistory.tsx
@@ -1,109 +1,174 @@
-import { StyleSheet, Image, Platform } from 'react-native';
+// App.js
+import React from "react";
+import { View, Text, StyleSheet, FlatList, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Ionicons } from "@expo/vector-icons";
 
-import { Collapsible } from '@/components/Collapsible';
-import { ExternalLink } from '@/components/ExternalLink';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import { IconSymbol } from '@/components/ui/IconSymbol';
+type gameData = {
+  date: string;
+  location: string;
+  time: string;
+  accuracy: string;
+  shots: number;
+};
 
-export default function TabTwoScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
-          style={styles.headerImage}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Explore</ThemedText>
-      </ThemedView>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the web version, press{' '}
-          <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
-          different screen densities
-        </ThemedText>
-        <Image source={require('@/assets/images/react-logo.png')} style={{ alignSelf: 'center' }} />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Custom fonts">
-        <ThemedText>
-          Open <ThemedText type="defaultSemiBold">app/_layout.tsx</ThemedText> to see how to load{' '}
-          <ThemedText style={{ fontFamily: 'SpaceMono' }}>
-            custom fonts such as this one.
-          </ThemedText>
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/versions/latest/sdk/font">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{' '}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook lets you inspect
-          what the user's current color scheme is, and so you can adjust UI colors accordingly.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{' '}
-          <ThemedText type="defaultSemiBold">components/HelloWave.tsx</ThemedText> component uses
-          the powerful <ThemedText type="defaultSemiBold">react-native-reanimated</ThemedText>{' '}
-          library to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The <ThemedText type="defaultSemiBold">components/ParallaxScrollView.tsx</ThemedText>{' '}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
-      </Collapsible>
-    </ParallaxScrollView>
+type gameHistory = {
+  month: string;
+  games: gameData[];
+};
+
+const data: gameHistory[] = [
+  {
+    month: "April",
+    games: [
+      {
+        date: "April 30, 2024",
+        location: "Waterloo, ON",
+        time: "18:27",
+        accuracy: "87%",
+        shots: 161,
+      },
+      {
+        date: "April 28, 2024",
+        location: "Waterloo, ON",
+        time: "12:27",
+        accuracy: "76%",
+        shots: 121,
+      },
+      {
+        date: "April 28, 2024",
+        location: "Waterloo, ON",
+        time: "12:27",
+        accuracy: "76%",
+        shots: 121,
+      },
+      {
+        date: "April 28, 2024",
+        location: "Waterloo, ON",
+        time: "12:27",
+        accuracy: "76%",
+        shots: 121,
+      },
+    ],
+  },
+  {
+    month: "March",
+    games: [
+      {
+        date: "March 28, 2024",
+        location: "Waterloo, ON",
+        time: "12:27",
+        accuracy: "76%",
+        shots: 121,
+      },
+      {
+        date: "March 28, 2024",
+        location: "Waterloo, ON",
+        time: "12:27",
+        accuracy: "76%",
+        shots: 121,
+      },
+      {
+        date: "March 28, 2024",
+        location: "Waterloo, ON",
+        time: "12:27",
+        accuracy: "76%",
+        shots: 121,
+      },
+    ],
+  },
+];
+
+const GameHistoryScreen = () => {
+  const renderGameItem = ({ item }: { item: gameData }) => (
+    <View style={styles.gameItem}>
+      <Text style={styles.text}>{item.date}</Text>
+      <Text style={styles.text}>{item.location}</Text>
+      <Text style={styles.text}>{item.accuracy}</Text>
+      <Text style={styles.text}>{item.shots}</Text>
+    </View>
   );
-}
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Game History</Text>
+      </View>
+
+      <View style={styles.totalGamesContainer}>
+        <Text style={styles.totalGamesText}>Total Games Played</Text>
+        <Text style={styles.totalGamesNumber}>591</Text>
+      </View>
+
+      <ScrollView>
+        {data.map((section, index) => (
+          <View key={index}>
+            <Text style={styles.monthHeader}>{section.month}</Text>
+            <FlatList
+              data={section.games}
+              renderItem={renderGameItem}
+              keyExtractor={(item, idx) => `${section.month}-${idx}`}
+            />
+          </View>
+        ))}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
+  container: {
+    flex: 1,
+    backgroundColor: "#F4F9F9",
   },
-  titleContainer: {
-    flexDirection: 'row',
-    gap: 8,
+  header: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+    backgroundColor: "#00C781",
+  },
+  headerTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "white",
+  },
+  totalGamesContainer: {
+    alignItems: "center",
+    paddingVertical: 20,
+    backgroundColor: "#D1F5F0",
+  },
+  totalGamesText: {
+    fontSize: 18,
+    color: "#333",
+  },
+  totalGamesNumber: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "#333",
+  },
+  monthHeader: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#333",
+    marginVertical: 10,
+    marginLeft: 16,
+  },
+  gameItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ddd",
+    marginHorizontal: 16,
+    backgroundColor: "white",
+    borderRadius: 8,
+    marginBottom: 8,
+  },
+  text: {
+    fontSize: 14,
+    color: "#333",
   },
 });
+
+export default GameHistoryScreen;
