86ddc5393d8d7413eae6af51f9d4e22ffd98f30c
evanferns <evanferns9@gmail.com>
2025-02-10 19:04:35 -0500

    Added ble for android

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {
   View,
   Text,
@@ -7,46 +7,93 @@ import {
   StyleSheet,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import DeviceModal from "../deviceConnectionModal";
+import useBLE from "../helper/useBLE";
 
 const HomeScreen = ({ navigation }: { navigation: any }) => {
+    const {
+      requestPermissions,
+      scanForPeripherals,
+      allDevices,
+      connectToDevice,
+      connectedDevice,
+      heartRate,
+      disconnectFromDevice,
+    } = useBLE();
+    const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+
+    const scanForDevices = async () => {
+      const isPermissionsEnabled = await requestPermissions();
+      if (isPermissionsEnabled) {
+        scanForPeripherals();
+      }
+    };
+
+    const hideModal = () => {
+      setIsModalVisible(false);
+    };
+
+    const openModal = async () => {
+      scanForDevices();
+      setIsModalVisible(true);
+    };
   return (
     <SafeAreaView style={styles.container}>
       {/* Summary Section */}
       <ScrollView>
-        <View style={styles.card}>
-          <Text style={styles.title}>Game History</Text>
-          <Text style={styles.stat}>Total Games Played: 591</Text>
-        </View>
+        {connectedDevice ? (
+          <>
+            <View style={styles.card}>
+              <Text style={styles.title}>Game History</Text>
+              <Text style={styles.stat}>Total Games Played: 591</Text>
+            </View>
 
-        <View style={styles.card}>
-          <Text style={styles.title}>Player Statistics</Text>
-          <Text style={styles.stat}>Total Shots Hit: 2067</Text>
-          <Text style={styles.stat}>Total Shots Missed: 98</Text>
-        </View>
+            <View style={styles.card}>
+              <Text style={styles.title}>Player Statistics</Text>
+              <Text style={styles.stat}>Total Shots Hit: 2067</Text>
+              <Text style={styles.stat}>Total Shots Missed: 98</Text>
+            </View>
+            {/* Most Recent Session */}
+            <View style={styles.card}>
+              <Text style={styles.title}>Most Recent Session</Text>
+              <Text>Date: April 28, 2024</Text>
+              <Text>Location: Waterloo, ON</Text>
+              <Text>Accuracy: 77%</Text>
+              <Text>Total Shots: 161</Text>
+            </View>
 
-        {/* Most Recent Session */}
-        <View style={styles.card}>
-          <Text style={styles.title}>Most Recent Session</Text>
-          <Text>Date: April 28, 2024</Text>
-          <Text>Location: Waterloo, ON</Text>
-          <Text>Accuracy: 77%</Text>
-          <Text>Total Shots: 161</Text>
-        </View>
-
-        {/* Start Session Button */}
+            {/* Start Session Button */}
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => navigation.navigate("Session")}
+            >
+              <Text style={styles.buttonText}>Start Session</Text>
+            </TouchableOpacity>
+          </>
+        ) : (
+          <Text>Please Connect to a Heart Rate Monitor</Text>
+        )}
         <TouchableOpacity
+          onPress={connectedDevice ? disconnectFromDevice : openModal}
           style={styles.button}
-          onPress={() => navigation.navigate("Session")}
         >
-          <Text style={styles.buttonText}>Start Session</Text>
+          <Text style={styles.buttonText}>
+            {connectedDevice ? "Disconnect" : "Connect"}
+          </Text>
         </TouchableOpacity>
+        <DeviceModal
+          closeModal={hideModal}
+          visible={isModalVisible}
+          connectToPeripheral={connectToDevice}
+          devices={allDevices}
+        />
       </ScrollView>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: "#E0FFF7"},
+  container: { flex: 1, backgroundColor: "#E0FFF7" },
   header: {
     flexDirection: "row",
     justifyContent: "center",
